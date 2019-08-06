0086b2a19f908cc2b370fd8d5408687369c8b190
Diego Mongay <diego.mongay@birkle-it.com>
2019-08-06 10:57:28 +0200

    updated views and controllers

--- client/src/main.js
+++ client/src/main.js
@@ -1,63 +1,89 @@
 (function () {
-    'use strict';
-    
-    angular.module('awesomeListApp', [])
+  'use strict';
+
+  angular.module('awesomeListApp', [])
     .controller('awesomeListAddController', awesomeListAddController)
     .controller('awesomeListShowController', awesomeListShowController)
+    .controler('doneListShowController', doneListShowController)
     .service('ShoppingListService', ShoppingListService);
-    
-    awesomeListAddController.$inject = ['ShoppingListService'];
-    function awesomeListAddController(ShoppingListService) {
-      var listAdder = this;
-    
-      listAdder.text = "";
-      listAdder.itemName = null;
-      listAdder.itemQuantity = null;
-    
-      listAdder.addItem = function () {
-        ShoppingListService.addItem(listAdder.itemName, listAdder.itemQuantity);
 
-        //reset inputs
-        listAdder.itemName = null;
-        listAdder.itemQuantity = null;
+  awesomeListAddController.$inject = ['ShoppingListService'];
+
+  function awesomeListAddController(ShoppingListService) {
+    var listAdder = this;
+
+    listAdder.text = "";
+    listAdder.priority = null;
+    listAdder.itemQuantity = null;
+
+    listAdder.addItem = function () {
+      ShoppingListService.addItem(listAdder.itemName, listAdder.priority);
+
+      //reset inputs
+      listAdder.itemName = null;
+      listAdder.priority = null;
+    };
+  }
+
+  awesomeListShowController.$inject = ['ShoppingListService'];
+
+  function awesomeListShowController(ShoppingListService) {
+    var showList = this;
+
+    showList.items = ShoppingListService.getItems();
+
+    showList.removeItem = function (itemIndex) {
+      ShoppingListService.removeItem(itemIndex);
+      ShoppingListService.addDoneItem(itemIndex);
+    };
+  }
+
+
+  doneListShowController.$inject = ['ShoppingListService'];
+
+  function doneListShowController(ShoppingListService) {
+    var doneList = this;
+
+    doneList.items = ShoppingListService.getDoneItems();
+
+  }
+
+
+  function ShoppingListService() {
+    var service = this;
+
+    // List of shopping items
+    var items = [];
+
+    var doneItems = [];
+
+    service.addItem = function (itemName, priority) {
+      var item = {
+        name: itemName,
+        priority: priority
       };
-    }
-    
-    
-    awesomeListShowController.$inject = ['ShoppingListService'];
-    function awesomeListShowController(ShoppingListService) {
-      var showList = this;
-    
-      showList.items = ShoppingListService.getItems();
-    
-      showList.removeItem = function (itemIndex) {
-        ShoppingListService.removeItem(itemIndex);
+      items.push(item);
+    };
+
+    service.addDoneItem = function (itemName, priority) {
+      var doneItem = {
+        name: itemName,
+        priority: priority
       };
-    }
-    
-    
-    function ShoppingListService() {
-      var service = this;
-    
-      // List of shopping items
-      var items = [];
-    
-      service.addItem = function (itemName, quantity) {
-        var item = {
-          name: itemName,
-          quantity: quantity
-        };
-        items.push(item);
-      };
-    
-      service.removeItem = function (itemIdex) {
-        items.splice(itemIdex, 1);
-      };
-    
-      service.getItems = function () {
-        return items;
-      };
-    }
-    
-    })();
-    
+      doneItems.push(doneItem);
+    };
+
+    service.removeItem = function (itemIdex) {
+      items.splice(itemIdex, 1);
+    };
+
+    service.getItems = function () {
+      return items;
+    };
+
+    service.getDoneItems = function () {
+      return doneItems;
+    };
+  }
+
+})();
